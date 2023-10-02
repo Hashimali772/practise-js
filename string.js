7fb2c1ab1069487a08f7360e5c374f2bacfbd398
hashimali772 <m.hashimali772@gmail.com>
2023-10-02 17:52:01 +0500

    some changes

--- string.js
+++ string.js
@@ -1,39 +1,41 @@
 // 1 Step : 
 
-// const fName = "hashim ";
-// const lName = "ali";
+const fName = "hashim ";
+const lName = "ali";
 
-// console.log(fName + lName); // this is a old method of strings.
+console.log(fName + lName); // this is a old method of strings.
 
-// console.log(`My first name is ${fName} and my last name is ${lName}`); // This is a new method of strings.
+console.log(`My first name is ${fName} and my last name is ${lName}`); // This is a new method of strings.
 
 // 2nd Step : 
 
-// const newString = new String("Hashim");
+const newString = new String("Hashim");
 
-// console.log(newString[2]); // Sqaure brackets to check the number of letter lenght.
-// console.log(newString.__proto__); 
+console.log(newString[2]); // Sqaure brackets to check the number of letter lenght.
+console.log(newString.__proto__); 
 
-// console.log(newString.length); // for checking the lenght of a string as it's name.
-// console.log(newString.charAt(2)); // for checking postion of alphabet through counting number.
-// console.log(newString.indexOf('s')); // for checking the postion of alphabet in number.
+console.log(newString.length); // for checking the lenght of a string as it's name.
+console.log(newString.charAt(2)); // for checking postion of alphabet through counting number.
+console.log(newString.indexOf('s')); // for checking the postion of alphabet in number.
 
-// const newAlpha = newString.substring(0, 4);
-// console.log(newAlpha); // use for print the string on dedicated number.
+const newAlpha = newString.substring(0, 4);
+console.log(newAlpha); // use for print the string on dedicated number.
 
-// const newGamma = newString.slice(-5, 5);
-// console.log(newGamma); // use for reverse the order
+// 3rd Step :
 
-// const spaceRemove = "  hashim  "
-// console.log(spaceRemove);
-// console.log(spaceRemove.trim()); // use for removing extra spaces.
+const newGamma = newString.slice(-5, 5);
+console.log(newGamma); // use for reverse the order
 
-// const url = "https//www.hashim.com"
+const spaceRemove = "  hashim  "
+console.log(spaceRemove);
+console.log(spaceRemove.trim()); // use for removing extra spaces.
 
-// console.log(url.replace('hashim', 'ali'));
-// console.log(url.includes('hashim'));
+const url = "https//www.hashim.com"
 
-// const a = "Hashim";
-// const b = "Ali"
+console.log(url.replace('hashim', 'ali'));
+console.log(url.includes('hashim'));
 
-// console.log(a.concat(',', b)); // to add a something else between two or more varaibles.
+const a = "Hashim";
+const b = "Ali"
+
+console.log(a.concat(',', b)); // to add a something else between two or more varaibles.
