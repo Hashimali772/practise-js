b1078b1082a9b5b165ea2805e587aa4919d6f99c
hashimali772 <m.hashimali772@gmail.com>
2023-10-17 01:03:42 +0500

    objects

---
+++ 01_objects.js
@@ -0,0 +1,20 @@
+// singleton or constructor
+
+// const myName = new Object(); singleton
+
+// Object literals
+
+const userAli = {
+    name: "Ali",
+    "fullName": "Muhammad Ali",
+    age: 20,
+    email: "muhammadali@google.com",
+    isLoggedin: true,
+    lastLoggedDays: ["Monday", "Tuesday", "Friday"],
+
+};
+
+console.log(userAli.age); // to get direct value.
+console.log(userAli["email"]); // to get value from the string type.
+console.log(userAli["fullName"]); // Same 
+
